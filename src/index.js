c20279dd8d01baca9b8eddf41b6898c5066a5994
Álvaro Prataviera Filho <alvaro.prataviera@bisolutions.com.br>
2017-12-03 01:10:00 -0600

    texto padrão de tweet

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ Notification.send('ugh');
 
 // Colocar um texto padrão para projetos novos, aprovados e reprovados: 
 //(Vereador tal criou projeto tal que está pendente de aprovação, confira aqui: link e dê aquele like se gostou do projeto)
-//(Foi aprovado projeto tal de vereador tal - link)
+//(Foi aprovado projeto tal de vereador tal - link Parabéns vereador tal pela aprovação!)
 //(Foi reprovado projeto tal de vereador tal - link)
